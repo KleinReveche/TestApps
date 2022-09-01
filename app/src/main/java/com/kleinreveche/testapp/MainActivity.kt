0e254096d05ea438848d086edceda7903c49105b
Klein Reveche <personal@kleinreveche.com>
2022-09-01 16:25:35 +0800

    fix: missing import

--- app/src/main/java/com/kleinreveche/testapp/MainActivity.kt
+++ app/src/main/java/com/kleinreveche/testapp/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
