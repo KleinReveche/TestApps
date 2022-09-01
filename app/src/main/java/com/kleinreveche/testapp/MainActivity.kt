ae99080b79236659c9ae11a3d21f4bd6a6a5b9c7
Klein Reveche <personal@kleinreveche.com>
2022-09-01 16:12:45 +0800

    fix: imports

--- app/src/main/java/com/kleinreveche/testapp/MainActivity.kt
+++ app/src/main/java/com/kleinreveche/testapp/MainActivity.kt
@@ -5,10 +5,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.ColorScheme
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.runtime.Composable
+import androidx.compose.material3.*
 import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
